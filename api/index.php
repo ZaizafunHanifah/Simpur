ed2a60857f39cff42b7403ced272112dfa9fb6af
ZaizafunHanifah <hanifah9158@gmail.com>
2026-02-04 22:36:08 +0700

    debug: enable display_errors and APP_DEBUG for Vercel troubleshooting

--- api/index.php
+++ api/index.php
@@ -17,6 +17,12 @@ require_once $vendorAutoload;
 
 // Create kernel and handle request
 $app = require $backendPath . '/bootstrap/app.php';
+ 
+// Enable error display temporarily for debugging on Vercel
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+putenv('APP_DEBUG=true');
 
 $kernel = $app->make(Illuminate\\Contracts\\Http\\Kernel::class);
 
