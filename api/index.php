7a8f332ee7f875991becf0c26573a66fd2d51753
ZaizafunHanifah <hanifah9158@gmail.com>
2026-02-04 11:03:36 +0700

    fix: add root composer.json for dependencies

--- api/index.php
+++ api/index.php
@@ -1,3 +1,5 @@
 <?php
 // Vercel entry point from Root
+// When composer.json is in root, vendor is also in root
+require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../frontend/public/index.php';
